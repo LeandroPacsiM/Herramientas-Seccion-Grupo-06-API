ef56370c2401e9a513b211492584e5dac3459e4c
Leandro Javier Pacsi Mamani <u19218678@utp.edu.pe>
2026-07-02 15:01:39 -0500

    fix: hacer StripeConfig condicional para que CI pase sin API key

--- src/main/java/com/llamatours/payment/config/StripeConfig.java
+++ src/main/java/com/llamatours/payment/config/StripeConfig.java
@@ -3,9 +3,11 @@ package com.llamatours.payment.config;
 import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty("stripe.secret.key")
 public class StripeConfig {
 
     @Value("${stripe.secret.key}")
